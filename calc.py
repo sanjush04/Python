6fb2542ee4c0e560c22949b51beb648b11dca011
SANJUSH <48871637+sanjush04@users.noreply.github.com>
2020-07-11 13:51:46 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,19 @@
+#This is a calculator
+
+import numpy as m
+
+print("Functions: *,+,-,/,mod,sqrt")
+s = input("Enter the function:")
+n = input("Enter the numbers:")
+arr = array(n)
+
+
+
+
+
+if s =='+':
+    print("The sum=",m.fsum(arr))
+    
+
+
+          
